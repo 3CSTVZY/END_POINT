2e22e62aede099bf970ad23d7957f2862ad0c100
3CSTVZY <corp.of.alexeev@gmail.com>
2022-10-20 13:44:33 +0300

    code

---
+++ Code/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+char[] body = ("rr31dy").ToCharArray(); 
+int count = 0;
+
+#region 
+foreach (char item in body)
+{ 
+if (!char.IsDigit(item))
+  {
+   count++;
+  }
+}
+#endregion
+
+char[] newArr = new char[count];
+count = 0;
+
+#region 
+foreach (char item in body) 
+{ 
+ if (!char.IsDigit(item))
+   {
+     newArr[count]=item;
+     count++;    
+   }
+}
+#endregion
+
+Console.Write("The result: ");
+Console.WriteLine(newArr);
